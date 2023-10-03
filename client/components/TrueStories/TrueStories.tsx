118aebf48c466db79246bbb3401b9dab746af146
Artem Zhuravlev <an3wer@yandex.ru>
2023-10-03 12:13:26 +0500

    update history image

--- client/components/TrueStories/TrueStories.tsx
+++ client/components/TrueStories/TrueStories.tsx
@@ -3,7 +3,7 @@ const TrueStories = () => {
   return (
     <div className=" flex flex-col justify-center -mt-10 space-y-10 md:space-y-16">
       <img src="/images/true_stories_header.png" className="w-full" alt="" />
-      <img src="/images/history-mini.png" className="w-full" alt="" />
+      <img src="/images/history-mini-min.png" className="w-full" alt="" />
     </div>
   )
 }
